05d413429922fc21117cee681063175043352107
zoheirsoftware <zoheir.s@yahoo.com>
2024-08-15 16:55:04 +0330

    two learn

---
+++ main.js
@@ -0,0 +1,14 @@
+class rectangel{
+    constructor(width,height)
+    {
+        this.width=width
+        this.height=height
+    }
+
+    show()
+    {
+console.log("width :"+this.width+" height :"+this.height+"")
+    }
+}
+var obj=new rectangel(20,30)
+obj.show()
