55e6c4ed73871b153ca05e3656cfa069406f1698
zoheirsoftware <zoheir.s@yahoo.com>
2024-08-16 16:50:41 +0330

    es 6

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-class rectangel{
+/* class rectangel{
     constructor(width,height)
     {
         this.width=width
@@ -12,3 +12,28 @@ console.log("width :"+this.width+" height :"+this.height+"")
 }
 var obj=new rectangel(20,30)
 obj.show()
+
+class colorrectangel extends rectangel{
+    constructor(width,height,color)
+    {
+        super(width,height)
+        this.color=color
+    }
+
+    render()
+    {
+        
+    }
+} */
+
+function sum(...args) {
+  return args
+    .filter(function (e) {
+      return typeof e === "number";
+    })
+    .reduce(function (prrv, caurr) {
+      return prrv + caurr;
+    });
+}
+
+console.log(sum(1,"a",null,20))
